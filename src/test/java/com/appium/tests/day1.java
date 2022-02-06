edce2f159d148aff8566e1b85e8d8267d049ee36
eren.cengel <eren.cengel@scalefocus.com>
2022-02-06 18:07:06 +0300

    first successfull execution

--- src/test/java/com/appium/tests/day1.java
+++ src/test/java/com/appium/tests/day1.java
@@ -7,25 +7,41 @@ import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class day1 {
 
     AppiumDriver<MobileElement> driver;
 
     @Test
-    public void test1(){
+    public void test1() throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         //we use android phone
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
         //version of android
-        desiredCapabilities.setCapability(MobileCapabilityType.VERSION,"11.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.VERSION,"8.0");
         //name of the device, if it real device we need to pass UUID parameter
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 5 API 30");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
         //either you specify app --> path/to/app.apk
         //or if app is already installed, you need to specify appActivity and appPackage
         //this info you can find in the internet, at work - ask to developers
         //appİNFO.APK file -- cruical
+
+        //Set your application's package name.
         desiredCapabilities.setCapability("appPackage","com.android.calculator2");
+
+        //Set your application's Mainactivity i.e. the LAUNCHER activity name.
         desiredCapabilities.setCapability("appActivity","com.android.calculator2.Calculator");
+
+        /*"http://localhost:4723/wd/hub --> address of the appium server. If you have appium server on the same computer...
+         *just use local host
+         * 4723 -> defualt port number
+         * we need to provide 2 parameters: URL of appium servers and desired capabilities
+         */
+        driver = new AppiumDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
+        Thread.sleep(3000);
+        driver.closeApp();
 
 
     }
