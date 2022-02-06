27f418b5aacf127ad65fc4af39283aa523c5f13a
eren.cengel <eren.cengel@scalefocus.com>
2022-02-06 17:15:54 +0300

    add driver

--- src/test/java/com/appium/tests/day1.java
+++ src/test/java/com/appium/tests/day1.java
@@ -1,4 +1,33 @@
 package com.appium.tests;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class day1 {
+
+    AppiumDriver<MobileElement> driver;
+
+    @Test
+    public void test1(){
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        //we use android phone
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        //version of android
+        desiredCapabilities.setCapability(MobileCapabilityType.VERSION,"11.0");
+        //name of the device, if it real device we need to pass UUID parameter
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 5 API 30");
+        //either you specify app --> path/to/app.apk
+        //or if app is already installed, you need to specify appActivity and appPackage
+        //this info you can find in the internet, at work - ask to developers
+        //appİNFO.APK file -- cruical
+        desiredCapabilities.setCapability("appPackage","com.android.calculator2");
+        desiredCapabilities.setCapability("appActivity","com.android.calculator2.Calculator");
+
+
+    }
+
 }
